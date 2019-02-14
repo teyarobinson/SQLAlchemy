9c402b494f7e6baf6a1007ea7e9475eec81cf90f
Teya Robinson <teya.robinson@gmail.com>
2019-02-13 23:45:07 -0500

    did my best

--- app.py
+++ app.py
@@ -1,6 +1,27 @@
 # 1. Import Flask
-from flask import Flask
 
+import numpy as np
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
+# Database Setup
+#################################################
+engine = create_engine("sqlite:///hawaii.sqlite")
+
+# reflect an existing database into a new model
+Base = automap_base()
+# reflect the tables
+Base.prepare(engine, reflect=True)
+
+# Save reference to the table
+Stations = Base.classes.station
+Measurement = Base.classes.measurement
+# Create our session (link) from Python to the DB
+session = Session(engine)
 
 # 2. Create an app
 app = Flask(__name__)
@@ -22,20 +43,39 @@ def index():
 
 @app.route("/api/v1.0/precipitation")
 def precipitation():
-  
+  # """Return a list of all preps and dates"""
+    # Query all preps and dates
+    results = session.query(Measurement.prcp).all()
+
+    # Convert list of tuples into normal list
+    all_temps = list(np.ravel(results))
+
+    return jsonify(all_temps)
 
 
 @app.route("/api/v1.0/stations")
 def stations():
+    results = session.query(Stations.name).all()
+
+    # Convert list of tuples into normal list
+    all_stations = list(np.ravel(results))
+
+    return jsonify(all_stations)
 
 @app.route("/api/v1.0/tobs")
 def tobs():   
+    results = session.query(Measurement.tobs).all()
 
-@app.route("/api/v1.0/<start>")
-def start():
+    # Convert list of tuples into normal list
+    all_tobs = list(np.ravel(results))
 
-@app.route("/api/v1.0/<start>/<end>")
-def end():
+    return jsonify(all_tobs)
+
+#@app.route("/api/v1.0/<start>")
+#def start():
+
+#@app.route("/api/v1.0/<start>/<end>")
+#def end():
 
 
 # 4. Define main behavior
